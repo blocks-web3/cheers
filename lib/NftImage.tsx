08781a94ebcd79f49a8231bf298211775831fe99
S.Koike <shomakoike0909@gmail.com>
2023-07-12 22:05:59 +0900

    css

--- lib/NftImage.tsx
+++ lib/NftImage.tsx
@@ -5,12 +5,12 @@ type Props = {
 };
 export default function NftImage(props: Props) {
   return (
-    <div
-      className="rounded-lg border-2 bg-slate-200 p-3 bg-gray"
+    <button
+      className="rounded-lg bg-slate-200 p-3 bg-gray hover:bg-slate-50"
       onClick={() => props.onClicked(props.imageUrl)}
     >
       <img className="rounded-lg" src={props.imageUrl} width={300}></img>
       <h3 className="text-slate-800">{props.nftName}</h3>
-    </div>
+    </button>
   );
 }
